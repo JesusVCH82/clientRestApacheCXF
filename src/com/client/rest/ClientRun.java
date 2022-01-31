dfeeb84207443bf643cc8e99bb89d27421e940ae
JesusVCH82 <jesusvazquezmac@gmai.com>
2022-01-30 21:16:21 -0600

    - Se crean los metodos pendientes para put, post y delete - Se documentan metodos y se hace implementacion en metodo main

--- src/com/client/rest/ClientRun.java
+++ src/com/client/rest/ClientRun.java
@@ -1,19 +1,39 @@
 package com.client.rest;
 
 import java.io.IOException;
-
 import org.apache.http.client.ClientProtocolException;
-
+import com.client.rest.models.Cliente;
 import com.client.rest.service.ClienteServiceImpl;
 
 public class ClientRun {
 
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		ClienteServiceImpl c = new ClienteServiceImpl();
-		c.getClients().forEach((cliente) -> {
-			System.out.println(cliente);
-		});
-		System.out.println(c.getClient(2));
+	public static void main(String[] args) {
+		ClienteServiceImpl clientService = new ClienteServiceImpl();
+		Cliente client = new Cliente(1, "jesus", "vazquez", 28, "JVCH1112221234", "Hidalgo 62", "554431981");
+		
+		try {	
+			//GET
+			System.out.println("GET: " + clientService.getClient(1));
+			
+			//GET
+			System.out.println("List of clients: ");
+			clientService.getClients().forEach((cliente) -> System.out.println(cliente));
+			
+			//POST
+			System.out.println("POST: " + clientService.saveClient(client));
+		
+			//PUT
+			System.out.println("PUT: " + clientService.updateClient(1, client));
+			
+			//DELETE
+			System.out.println("DELETE: " + clientService.deleteClient(1));	
+			
+		} catch (ClientProtocolException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		
 	}
 }
--- src/com/client/rest/service/ClienteServiceImpl.java
+++ src/com/client/rest/service/ClienteServiceImpl.java
@@ -3,10 +3,15 @@ package com.client.rest.service;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import javax.ws.rs.core.MediaType;
 import org.apache.cxf.helpers.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import com.client.rest.models.Cliente;
@@ -14,8 +19,14 @@ import com.google.gson.Gson;
 
 public class ClienteServiceImpl {
 	
-	private static final String PATH_API = "http://192.168.1.80:8080/serviceREST-JAX-RS/api/v1/clientes";
+	private static final String PATH_API = "http://localhost:8080/serviceREST-JAX-RS/api/v1/clientes";
 	
+	/**
+	 * Metodo para obtener todos los clientes
+	 * GET /clientes
+	 * @return
+	 * @throws ClientProtocolException, IOException 
+	 */
 	public List<Cliente> getClients() throws ClientProtocolException, IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet getResourceClients = new HttpGet(PATH_API);
@@ -24,31 +35,83 @@ public class ClienteServiceImpl {
 		String clientsString = IOUtils.toString(response.getEntity().getContent());
 		//String se convierte a Arreglo de tipo Cliente
 		Cliente[]  clientes = new Gson().fromJson(clientsString, Cliente[].class);
+		//Se cierra conexion a la peticion
+		httpclient.close();
 		return Arrays.asList(clientes);
-		
 	}
 	
+	/**
+	 * Metodo para obtener un cliente por id
+	 * GET /clientes/{id}
+	 * @param id
+	 * @return
+	 * @throws ClientProtocolException, IOException 
+	 */
 	public Cliente getClient(int id) throws ClientProtocolException, IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet getResourceClient = new HttpGet(PATH_API + "/" + String.valueOf(id));
 		HttpResponse response = httpclient.execute(getResourceClient);
 		String clientString = IOUtils.toString(response.getEntity().getContent());
 		Cliente cliente = new Gson().fromJson(clientString, Cliente.class);
+		httpclient.close();
 		return cliente;
 	}
 	
-	public String saveClient(Cliente cliente) {
-		return null;
+	/**
+	 * Metodo para crear un nuevo cliente
+	 * POST /clientes
+	 * @param cliente
+	 * @return
+	 * @throws ClientProtocolException, IOException 
+	 */
+	public String saveClient(Cliente cliente) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost saveClient = new HttpPost(PATH_API);
+		saveClient.setHeader("Content-Type", MediaType.APPLICATION_JSON);
+		String jsonClient = new Gson().toJson(cliente);
+		StringEntity entityCliente = new StringEntity(jsonClient);
+		saveClient.setEntity(entityCliente);
+		HttpResponse responseClient = httpclient.execute(saveClient);
+		String clientString = IOUtils.toString(responseClient.getEntity().getContent());
+		httpclient.close();
+		return clientString;
 		
 	}
 	
-	public String updateClient(int id, Cliente cliente) {
-		return null;
-
+	/**
+	 * Metodo para actualizar un cliente por id
+	 * PUT /clientes/{id}
+	 * @param id
+	 * @param cliente
+	 * @return
+	 * @throws ClientProtocolException, IOException 
+	 */
+	public String updateClient(int id, Cliente cliente) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPut updateClient = new HttpPut(PATH_API + "/" + String.valueOf(id));
+		updateClient.setHeader("Content-Type", MediaType.APPLICATION_JSON);
+		String jsonClient = new Gson().toJson(cliente);
+		StringEntity entityCliente = new StringEntity(jsonClient);
+		updateClient.setEntity(entityCliente);
+		HttpResponse responseClient = httpclient.execute(updateClient);
+		String clientString = IOUtils.toString(responseClient.getEntity().getContent());
+		httpclient.close();
+		return clientString;
 	}
 	
-	public String deleteClient(int id) {
-		return null;
-		
+	/**
+	 * Metodo para eliminar un cliente por id
+	 * DELETE /clientes/{id}
+	 * @param id
+	 * @return
+	 * @throws ClientProtocolException, IOException
+	 */
+	public String deleteClient(int id) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpDelete deleteClient = new HttpDelete(PATH_API + "/" + String.valueOf(id));
+		HttpResponse responseClient = httpclient.execute(deleteClient);
+		String clientString = IOUtils.toString(responseClient.getEntity().getContent());
+		httpclient.close();
+		return clientString;	
 	}
 }
